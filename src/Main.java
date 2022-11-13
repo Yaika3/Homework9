69e0a177f086d5cc18c6bf1bf6b3be9fb44d081b
max <kabinidi@mail.ru>
2022-11-13 17:48:24 +0300

    Дз Массивы ч1 (дополнено)

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,37 @@ public class Main {
             System.out.println(weight[i]);
 
         }
+        //task 1
+        int [] one = new int [3];
+        one[0]=1;
+        one[1]=2;
+        one [2]=3;
+        for (int i =0 ; i < one.length ;i++){   // не могу понять как решить...
+            if ( one[0]% 2==0 ){one[0]= one[0]+1;
+                System.out.println(one[0]);}
+        if (one[2] % 2 ==0 ){one[2]=one[2]+ 1;}}
 
+
+
+
+
+
+        System.out.print(one[0]+","+ one[1]+','+ one[2] );
+
+
+        //task 2
+        double [] two = new double[] {1.57, 7.654, 9.986};
+        System.out.print( two[0]+","+ two[1]+','+ two[2] );
+
+
+
+        //task 3
+        int [] three = new int[] {5,8};
+        System.out.print( three [0] +","+ three[1] );
+
+        System.out.println('\n');
+        System.out.println(one[2]+","+ one[1]+','+ one[0]);
+        System.out.println( two[2]+","+ two[1]+','+ two[0] );
+        System.out.println( three[1]+","+ three[0] );
     }
 }
